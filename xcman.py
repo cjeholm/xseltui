1c8e5c4ce5383f4c1e10b35e0084b08107d8b091
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-03-26 15:54:40 +0100

    Set all colors

--- xcman.py
+++ xcman.py
@@ -10,10 +10,12 @@ def main(stdscr):
 
     curses.use_default_colors()     # use terminal colors
     curses.curs_set(0)              # hide the cursor
-    curses.init_pair(1, 14, -1)     # color pair 1: color no 14 and transparent background
-    curses.init_pair(2, 3, -1)      # color pair 2: color no 7
-    curses.init_pair(3, 15, -1)
-    curses.init_pair(4, 7, -1)
+
+    # Set colors
+    i = 0
+    while i < 16:
+        curses.init_pair(i, i, -1)
+        i += 1
 
     xclipHistory = []
     print(xclipHistory)
@@ -30,23 +32,23 @@ def main(stdscr):
             xclipHistory.pop(-1)
 
         stdscr.clear()                  # clear screen
-        stdscr.addstr(1, 5, str(xclipLatest), curses.color_pair(4)) # Active clipboard
+        stdscr.addstr(1, 5, str(xclipLatest), curses.color_pair(7)) # Active clipboard
 
         lineNumber = 0
         while lineNumber < historyLength:
             if lineNumber == 0:
-                stdscr.addstr(1 + lineNumber, 2, ">", curses.color_pair(2))
+                stdscr.addstr(1 + lineNumber, 2, ">", curses.color_pair(3))
             else:
-                stdscr.addstr(1 + lineNumber, 2, str(lineNumber), curses.color_pair(1))
+                stdscr.addstr(1 + lineNumber, 2, str(lineNumber), curses.color_pair(14))
             lineNumber += 1
 
         lineNumber = 1
         for item in xclipHistory:
-            stdscr.addstr(1 + lineNumber, 5, str(item), curses.color_pair(3))
+            stdscr.addstr(1 + lineNumber, 5, str(item), curses.color_pair(15))
             lineNumber += 1
 
-        stdscr.addstr(12, 2, "Keys (1-9) selects a buffer", curses.color_pair(1))
-        stdscr.addstr(13, 2, "(x) toggles Delete Mode", curses.color_pair(1))
+        stdscr.addstr(12, 2, "Keys (1-9) selects a buffer", curses.color_pair(14))
+        stdscr.addstr(13, 2, "(x) toggles Delete Mode", curses.color_pair(14))
 
         stdscr.refresh()
         time.sleep(0.5)
